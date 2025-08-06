8470c7eca264fe4481a11e3709be940904529039
JupiterMack <mackjupiter@gmail.com>
2025-08-06 22:10:35 +0200

    2 init

--- hosts/nixos/variables.nix
+++ hosts/nixos/variables.nix
@@ -16,7 +16,7 @@
       + config.var.username
       + "/.config/nixos"; # The path of the nixos configuration directory
 
-    keyboardLayout = "en";
+    keyboardLayout = "fr";
 
     location = "Paris";
     timeZone = "Europe/Paris";
