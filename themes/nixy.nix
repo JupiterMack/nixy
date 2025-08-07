93e202a6308f86e3cdc190747d2be6a69955cd26
JupiterMack <mackjupiter@gmail.com>
2025-08-07 21:18:41 +0200

    bruh

--- themes/nixy.nix
+++ themes/nixy.nix
@@ -83,7 +83,7 @@
 
     polarity = "dark";
     image = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/black-and-white-forest_minimalist_black-and-white.png";
+      url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/it-is-some-waves_dark.png";
       sha256 = "sha256-MOlLRQonZ6UAaSJlysjL8snxnMrSFH9VOLrjXaU82Kw=";
     };
   };
