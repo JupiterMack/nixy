392b2d2df8abb0fec483ed2c769ee7bf9619cb24
JupiterMack <mackjupiter@gmail.com>
2025-08-07 20:27:47 +0200

    o

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -38,8 +38,6 @@
     ../../home/system/walker
     ../../home/system/udiskie
     # ../../home/system/clipman
-
-    ./secrets # CHANGEME: You should probably remove this line, this is where I store my secrets
   ];
 
   home = {
